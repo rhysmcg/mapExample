46cc5c8464f8a81a72c2c655c80717cb3c6040d6
rhysmcg <rhysmcgoldrick@gmail.com>
2021-12-14 12:36:49 +1000

    Separated code from HTML to JS files

---
+++ js/mapProperties.js
@@ -0,0 +1,16 @@
+// Change the Map Properties Here
+
+var start_coordinates = [37.31885, -80.21857]
+var start_zoomLevel = 5
+var minZoom = 4
+
+// Polygon Styles
+var PolygonStyles = {
+  
+  selectedFillColor:'#FFA500',
+  selectedStrokeColor:'#FFA500',
+  
+  unselectedFillColor:'#0000FF',
+  unselectedStrokeColor:'#0000FF'
+  
+}
