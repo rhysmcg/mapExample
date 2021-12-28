b43ae444fbb51fb90dccf39328477a610b8bb708
rhysmcg <rhysmcgoldrick@gmail.com>
2021-12-28 15:23:44 +1000

    Added Google Maps Stuff

--- js/mapProperties.js
+++ js/mapProperties.js
@@ -1,16 +1,26 @@
 // Change the Map Properties Here
 
-var start_coordinates = [37.31885, -80.21857]
+var start_coordinates = { lat: 37.31885, lng: -80.21857 }
 var start_zoomLevel = 5
 var minZoom = 4
 
+
+var mobile_start_coordinates = { lat: 37.31885, lng: -80.21857 }
+var mobile_start_zoomLevel = 4
+var mobile_minZoom = 3
+
 // Polygon Styles
 var PolygonStyles = {
-  
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillOpacity: 0.35,
   selectedFillColor:'#FFA500',
   selectedStrokeColor:'#FFA500',
-  
   unselectedFillColor:'#0000FF',
   unselectedStrokeColor:'#0000FF'
-  
 }
+
+const d = new Date();
+let year = d.getFullYear();
+var AttributionInfo = 'Eclipse data © 2017-' + year + ' eclipse2017.org dba eclipse2024.org.<br>All Rights Reserved.'
+
